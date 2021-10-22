30637194a1303ae65a2a51410208d876c647df40
DESKTOP-HPT2DJH\qoodo <qoodog12>
2021-10-22 11:26:31 +0900

    MainAction , MainController commit

---
+++ MoHang/src/mohang/action/main/MainAction.java
@@ -0,0 +1,20 @@
+package mohang.action.main;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import mohang.action.Action;
+import mohang.action.ActionForward;
+
+public class MainAction implements Action {
+	
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ActionForward forward = new ActionForward();
+		
+		forward.setRedirect(false);
+		forward.setPath("/Main.jsp");
+		return forward;
+	}
+}
