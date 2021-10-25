b649d21b08807e68a3bb34511743db2c496b5243
DESKTOP-5JLN7TS\Rois <zhsks9709@gmail.com>
2021-10-25 12:37:11 +0900

    커커커컼커커밋

--- MoHang/src/mohang/controller/ReviewController.java
+++ MoHang/src/mohang/controller/ReviewController.java
@@ -1,6 +1,8 @@
 package mohang.controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,6 +11,8 @@ import javax.servlet.http.HttpServletResponse;
 
 import mohang.action.Action;
 import mohang.action.ActionForward;
+import mohang.action.main.MainAction;
+import mohang.action.review.ReviewListAction;
 
 @WebServlet("/Review/*")
 public class ReviewController extends HttpServlet {
@@ -22,6 +26,23 @@ public class ReviewController extends HttpServlet {
     	String command = requestURI.substring(contextPath.length()+8);
     	Action action = null; 
     	ActionForward forward = null;
+    	if(command.equals("ReviewList.do")) {
+    		action = new ReviewListAction();
+    		try {
+				forward = action.execute(request, response);	
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+    	}
+    	
+    	if(forward !=null) {
+    		if(forward.isRedirect()) {
+    			response.sendRedirect(forward.getPath());
+    		}else {
+    			RequestDispatcher dispatcher = request.getRequestDispatcher(forward.getPath());
+    			dispatcher.forward(request, response);
+    		}
+    	}
     	
     }
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
