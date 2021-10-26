345826fcd69e2d3d2c34248a35f5be0ad93bc759
DESKTOP-HPT2DJH\qoodo <qoodog12>
2021-10-26 17:29:33 +0900

    한번더 커밋

--- MoHang/WebContent/resources/js/search.js
+++ MoHang/WebContent/resources/js/search.js
@@ -16,12 +16,15 @@ $(function() {
 	//검색 삭제 필터 이벤트
 	var filterbtns = $('.filter-btn');
 	$.each(filterbtns, function(i, item) {
-		console.log(item);
 		$(item).click(function(e) {
 			$(item).hide();
 		})
 	})
-	//검색 추가 필터 이벤트 
-	var inputs = $('input [type=raido]');
-	console.log(inputs);
+	//검색 추가 필터 이벤트
+	var inputs = $('input[type=raido]');
+	$('input[type=radio]').change(function() {
+		console.log(this.value+this.name);
+	})
+		
+	
 })
